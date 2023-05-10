cd0f0f588871441b22278da9513eee803230dedf
flo <floriane.montanari@bayer.com>
2023-05-10 12:06:48 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mlr-xai-selfies',
+    version='1.0',
+    packages=['xai_selfies'],
+    url='',
+    license='',
+    author='Floriane Montanari',
+    author_email='floriane.montanari@gmail.com',
+    description=''
+)
