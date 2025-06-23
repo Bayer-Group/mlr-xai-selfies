28c80f80f3111155f5f82dff387cac00cf8c0f76
Kerrin Janßen <kerrinjanssen@Mac.fritz.box>
2025-06-23 11:19:22 +0200

    added scaled heatmaps; added const. histogram plotting; added train/test handling

--- xai_selfies/plotting_helper.py
+++ xai_selfies/plotting_helper.py
@@ -2,15 +2,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Draw
+from IPython.display import Image
+from matplotlib.colors import LinearSegmentedColormap, TwoSlopeNorm
+import os
+import ast
+import matplotlib.colors as mcolors
+import ast
+from matplotlib.colorbar import ColorbarBase
+from xai_selfies.get_indices import *
 
 
-def plot_2D(LEGEND, placeLegend, Xaxes, Yaxes, Xlable, Ylable, SAVEDIR, color, include_line=True, line_style='-'):
+
+def plot_2D(LEGEND, placeLegend, Xaxes, Yaxes, Xlable, Ylable, SAVEDIR, color, header=None, include_line=True, line_style='-'):
     plt.rcParams['font.family'] = 'arial'
     plt.plot(Xaxes, Yaxes, '.', color=color, markersize=10, linestyle=line_style, alpha=0.5)
     plt.xlabel(Xlable, fontsize=18)
     plt.ylabel(Ylable, fontsize=18)
     if include_line:
         plt.plot((np.min(Xaxes),np.max(Xaxes)), (np.min(Yaxes), np.max(Yaxes)), color='gray')
+    if header is not None:
+        plt.title(header, fontsize=20)
+        #for correct saving
+        safe_header = header.replace(" ", "_").replace("/", "-")
+        directory, filename = os.path.split(SAVEDIR)
+        name, ext = os.path.splitext(filename)
+        new_filename = f"{name}_{safe_header}{ext}"
+        SAVEDIR = os.path.join(directory, new_filename)
     plt.tick_params(labelsize=16)
     plt.legend(LEGEND, fontsize=16, loc=placeLegend)
     plt.savefig(SAVEDIR, dpi=300, bbox_inches='tight')
@@ -53,6 +72,36 @@ def plot_histogram(dataset1, dataset2, colum_of_interest, x_Label, y_Label, data
     plt.tick_params(labelsize=16)
     plt.xticks(bins[:-1], labels=bin_labels)
     plt.savefig(SAVEDIR, dpi=300, bbox_inches='tight')
+    plt.show()
+
+def plot_histogram_one_dataset(data, colum_of_interest, label, color, attr_method, save_dir, header):
+
+    #get standard deviation
+    std_dev = np.std(data[colum_of_interest])
+
+    # color
+    base_color = mcolors.to_rgba(color) 
+    color_with_alpha = base_color[:3] + (0.5,)
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.hist(data[colum_of_interest], bins=30, color=color_with_alpha, edgecolor=color, label=f"{attr_method} \n(Standard Deviation: {std_dev:.2f})")  
+    
+    if header is not None:
+        plt.title(header, fontsize=20)
+        #to save correctly
+    else:
+        header = "plot"
+
+    plt.rcParams['font.family'] = 'arial'
+    plt.xlabel(label, fontsize=18)
+    plt.ylabel('MMP Count', fontsize=18)
+    plt.legend(fontsize=16, loc='upper left')
+
+    # Save plot
+    filename = f"{attr_method.replace(' ', '_').lower()}_histogram_{header.replace(' ', '_').lower()}.png"
+    save_path = os.path.join(save_dir, filename)
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
     plt.show()
 
 def replace_nan_with_zero(vector_str):
@@ -103,3 +152,113 @@ def get_r2_and_summed_data_attributions_const(data, pred_column_1, pred_column_2
     r2_const = np.corrcoef(data['delta_prediction'], data['delta_sum_const_contributions_' + attribution_type])[0,1]**2
 
     return r2_const, data
+
+def plot_MMP_correlations(data_MMPs, attr_method, color, working_dir, Target_Column_Name, header=None):
+
+    r2_whole, r2_fragment, data_MMPs = get_r2_and_summed_data_attributions(data_MMPs, 'predictions_1', 'predictions_2', attr_method + '_1', attr_method + '_2', attr_method, 'unmatched_atom_index_1' , 'unmatched_atom_index_2')
+
+    plot_2D(['$r^2$(' + attr_method + ') = ' + str(f"{r2_whole:.2f}")], 'upper left', data_MMPs['delta_prediction'], data_MMPs['delta_sum_' + attr_method],
+            '$\Delta$Predictions MMP', '$\Delta$Attributions MMP (whole Mol)', working_dir + 'PREDvsCONTRIBUTIONSwhole' + attr_method + '.png', 
+            color, header,
+            include_line=False, line_style='None')
+
+    plot_2D(['$r^2$(' + attr_method + ') = ' + str(f"{r2_fragment:.2f}")], 'upper left', data_MMPs['delta_prediction'], data_MMPs['delta_sum_fragment_contributions_' + attr_method],
+            '$\Delta$Predictions MMP', '$\Delta$Attributions MMP (Fragment)', working_dir + 'PREDvsCONTRIBUTIONSfragment' + attr_method + '.png', 
+            color, header,
+            include_line=False, line_style='None')
+
+
+    data_MMPs['delta_target'] = data_MMPs[Target_Column_Name + '_1'] - data_MMPs[Target_Column_Name + '_2']
+
+    r2 = np.corrcoef(data_MMPs['delta_target'], data_MMPs['delta_sum_' + attr_method])[0,1]**2
+
+    plot_2D(['$r^2$(' + attr_method + ') = ' + str(f"{r2:.2f}")], 'upper left', data_MMPs['delta_target'], data_MMPs['delta_sum_' + attr_method],
+            '$\Delta$Target MMP', '$\Delta$Attributions MMP (whole Mol)', working_dir + 'EXPvsCONTRIBUTIONSwhole' + attr_method + '.png', 
+            color, header,
+            include_line=False, line_style='None')
+    
+    return data_MMPs
+
+def plot_const_histogram(data_MMPs, attr_method, color, working_dir, header=None):
+    data_MMPs['const_indices_1'] = data_MMPs.apply(lambda row: get_unselected_atom_indices(row['smiles_1'], row['unmatched_atom_index_1']), axis=1)
+    data_MMPs['const_indices_2'] = data_MMPs.apply(lambda row: get_unselected_atom_indices(row['smiles_2'], row['unmatched_atom_index_2']), axis=1)
+
+    r2_const, data_MMPs = get_r2_and_summed_data_attributions_const(data_MMPs, 'predictions_1', 'predictions_2', attr_method + '_1', attr_method + '_2', attr_method, 'const_indices_1' , 'const_indices_2')
+
+    plot_histogram_one_dataset(data_MMPs, 'delta_sum_const_contributions_' + attr_method, '$\Delta$Attributions MMP (Constant)', color, attr_method, working_dir, header)
+    
+    return data_MMPs
+
+def generate_heatmap(data, index, output_dir, smiles_column, attribution_column, ID_column):
+    '''
+    Red is positive while blue is negative (to be consistant with PIXIE)
+    '''
+    smiles = data[smiles_column][index]
+    attributions = data[attribution_column][index]#ast.literal_eval()
+    mol_id = data[ID_column][index]
+
+    #vmax = data[attribution_column].abs().max() * 0.7
+    #evaluated_series = data[attribution_column].apply(ast.literal_eval)
+    vmax = data[attribution_column].apply(lambda lst: max(map(abs, lst))).max() * 0.7
+    vmin = -vmax
+
+    mol = Chem.MolFromSmiles(smiles, sanitize=False)
+    Chem.SanitizeMol(mol)#to keep the explicit hydrogens
+
+    # Draw similarity map
+    atom_colors = {}
+    for atom in mol.GetAtoms():
+        idx = atom.GetIdx()
+        weight = attributions[idx] if idx < len(attributions) else 0.0  # Default weight as 0.0 if atom index not found
+        color, cmap = get_color(weight, '#10384f', '#ffffff', '#9C0D38', vmin, vmax)
+        atom_colors[idx] = color
+        atom.SetProp("_displayColor", ','.join(map(str, color)))
+    
+    d = Draw.MolDraw2DCairo(1800, 1800)
+    d.DrawMolecule(mol,highlightAtoms=list(atom_colors.keys()),highlightAtomColors=atom_colors)#, highlightBonds=[]
+    d.FinishDrawing()
+
+    # Create a color bar
+    fig, ax = plt.subplots(figsize=(6, 1))
+    fig.subplots_adjust(bottom=0.5)
+
+    norm = TwoSlopeNorm(vmin=vmin, vcenter=0.0, vmax=vmax)
+    cbar = ColorbarBase(ax, norm=norm, cmap=cmap, orientation='horizontal')  #
+
+    tick_positions = [vmin, vmax]
+    tick_labels = [vmin, vmax]
+    cbar.set_ticks(tick_positions)
+    cbar.set_ticklabels(tick_labels)
+
+    plt.savefig(os.path.join(output_dir, 'Legend.png'), dpi=300, bbox_inches='tight')
+    plt.close()
+
+    # Define and write output path
+    filename = f"{mol_id}_{attribution_column}.png"
+    output_path = os.path.join(output_dir, filename)
+    with open(output_path, "wb") as f:
+        f.write(d.GetDrawingText())
+
+    return Image(filename=output_path)
+
+def get_color(weight, neg_color, neut_color, pos_color, vmin, vmax):
+    """
+    Creates a custom colormap from black to red and colors the bonds accordingly.
+
+    Keyword arguments:
+    -- weight: Weight of the bond to be colored.
+    -- neg_color: Color with which the decrease of the target variable should be described.
+    -- neut_color: Color with which no influence of the target variable should be described.
+    -- pos_color: Color with which the increase of the target variable should be described.
+    -- vmin: Lower bound to normalize the colourcoding on the bond weights.
+    -- vmax: Upper bound to normalize the colourcoding on the bond weights.
+
+    Returns:
+    -- tuple(rgba[:3]): Colour of the bond.
+    -- cmap: Custom color map.
+    """
+    norm = TwoSlopeNorm(vmin=vmin, vcenter=0.0, vmax=vmax)
+    colors = [neg_color, neut_color, pos_color] # 'dimgray'
+    cmap = LinearSegmentedColormap.from_list("custom_cmap", colors)
+    rgba = cmap(norm(weight))
+    return tuple(rgba[:3]), cmap
